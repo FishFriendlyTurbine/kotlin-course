25e49916bbbb4924469f8040475c1a4ee45d89c7
Fish <klimenko.product@gmail.com>
2025-09-07 01:29:23 +0500

    Change name and message again

---
+++ src/main/kotlin/Lession 2 homework.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main() {
+
+    println("Good luck, Guys!")
+
+    for (i in 1..10) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
+    }
+}
